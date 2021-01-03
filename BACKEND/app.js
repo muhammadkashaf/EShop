f9374a865bae71f0250e666755a0ac87105e7b93
muhammad kashaf <kashafahmed72@gmail.com>
2021-01-03 12:41:20 +0500

    created api routes and schemas and refractor code

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,44 +1,47 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors())
 
 //middleware
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
-require("dotenv/config");
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
 const api = process.env.API_URL;
 
-app.get(`${api}/products`, (req, res) => {
-  const products = {
-    id: 1,
-    name: "dev",
-    image: "some_url",
-  };
-  res.send(products);
-});
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-app.post(`${api}/products`, (req, res) => {
-  const newProducts = req.body;
-  console.log(newProducts);
-  res.send(newProducts);
-});
-
-
-
-mongoose
-  .connect(process.env.CONNECTION_STRING, {
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: 'eshop-database'
-  })
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
 
+//Server
+app.listen(3000, ()=>{
 
-
-app.listen(3000, () => {
-  console.log(api);
-  console.log("server is running");
-});
+    console.log('server is running http://localhost:3000');
+})
