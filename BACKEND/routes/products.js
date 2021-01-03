3ad707c11563fc7e1d148c54c672cbd0efb0938d
muhammad kashaf <kashafahmed72@gmail.com>
2021-01-03 15:47:54 +0500

    Get a Product & List of Products REST API

--- BACKEND/routes/products.js
+++ BACKEND/routes/products.js
@@ -4,12 +4,21 @@ const express = require("express");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find();
+  const productList = await Product.find().select('name image -_id');
 
   if (!productList) {
     res.status(500).json({ success: false });
   }
   res.send(productList);
+});
+
+router.get(`/:id`, async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(500).json({ success: false });
+  }
+  res.send(product);
 });
 
 router.post(`/`, async (req, res) => {
