77d403535b9aaffceb09ef31b3be88e07320affc
muhammad kashaf <kashafahmed72@gmail.com>
2021-01-03 16:15:13 +0500

    Update a Product REST API

--- BACKEND/routes/products.js
+++ BACKEND/routes/products.js
@@ -3,10 +3,11 @@ const { Category } = require("../models/category");
 const express = require("express");
 const router = express.Router();
 
+
+
+//Get Product REST API
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find()
-    .select("name image -_id")
-    .populate("category");
+  const productList = await Product.find();
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -15,13 +16,18 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get(`/:id`, async (req, res) => {
-  const product = await Product.findById(req.params.id).populate("category");
+  const product = await Product.findById(req.params.id);
 
   if (!product) {
     res.status(500).json({ success: false });
   }
   res.send(product);
 });
+
+
+
+
+//Add Product REST API
 
 router.post(`/`, async (req, res) => {
   const category = await Category.findById(req.body.category);
@@ -47,4 +53,36 @@ router.post(`/`, async (req, res) => {
 
   res.send(product);
 });
+
+
+
+//Update Product REST API
+
+router.put("/:id", async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid Category");
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  );
+
+  if (!product) return res.status(500).send("the product cannot be updated!");
+
+  res.send(product);
+});
+
 module.exports = router;
