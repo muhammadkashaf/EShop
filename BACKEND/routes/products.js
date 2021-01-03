519d072761667be55847756b05f9f5e846a34123
muhammad kashaf <kashafahmed72@gmail.com>
2021-01-03 17:01:56 +0500

    Get Featured Products REST API

--- BACKEND/routes/products.js
+++ BACKEND/routes/products.js
@@ -114,4 +114,16 @@ router.get(`/get/count`, async (req, res) => {
   });
 });
 
+
+//Get Featured Products REST API
+router.get(`/get/featured/:count`, async (req, res) => {
+  const count = req.params.count ? req.params.count : 0;
+  const products = await Product.find({ isFeatured: true }).limit(+count);
+
+  if (!products) {
+    res.status(500).json({ success: false });
+  }
+  res.send(products);
+});
+
 module.exports = router;
