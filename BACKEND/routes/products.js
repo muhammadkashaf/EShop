2bdd346dad0e1db3045aaff27269a266956c7d1a
muhammad kashaf <kashafahmed72@gmail.com>
2021-01-03 15:53:06 +0500

    Population of a Categoryin a Get Product API

--- BACKEND/routes/products.js
+++ BACKEND/routes/products.js
@@ -4,7 +4,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find().select('name image -_id');
+  const productList = await Product.find()
+    .select("name image -_id")
+    .populate("category");
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -13,7 +15,7 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get(`/:id`, async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id).populate("category");
 
   if (!product) {
     res.status(500).json({ success: false });
