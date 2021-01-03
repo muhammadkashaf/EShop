70c2cce8451645d40b4c9c56093ae4f3ec0749ad
muhammad kashaf <kashafahmed72@gmail.com>
2021-01-03 13:33:30 +0500

    get category details

--- BACKEND/routes/categories.js
+++ BACKEND/routes/categories.js
@@ -2,14 +2,33 @@ const { Category } = require("../models/category");
 const express = require("express");
 const router = express.Router();
 
+
+
 router.get("/", async (req, res) => {
   const categoryList = await Category.find();
 
   if (!categoryList) {
     res.status(500).json({ success: false });
   }
-  res.send(categoryList);
+  res.status(200).send(categoryList);
 });
+
+
+
+
+router.get("/:id", async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    res
+      .status(500)
+      .json({ message: "The Category with the given ID was not found" });
+  }
+  res.status(200).send(category);
+});
+
+
+
 
 router.post("/", async (req, res) => {
   let category = new Category({
@@ -23,6 +42,9 @@ router.post("/", async (req, res) => {
   if (!category) return res.status(404).send("the category cannot be created!");
   res.send(category);
 });
+
+
+
 
 router.delete("/:id", (req, res) => {
   Category.findByIdAndRemove(req.params.id)
