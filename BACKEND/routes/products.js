a8cfeeb682da4510adecb5389509a37040737e81
muhammad kashaf <kashafahmed72@gmail.com>
2021-01-03 14:16:28 +0500

    Post a new Product REST API

--- BACKEND/routes/products.js
+++ BACKEND/routes/products.js
@@ -1,31 +1,39 @@
-const {Product} = require('../models/product');
-const express = require('express');
+const { Product } = require("../models/product");
+const { Category } = require("../models/category");
+const express = require("express");
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const productList = await Product.find();
+router.get(`/`, async (req, res) => {
+  const productList = await Product.find();
 
-    if(!productList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(productList);
-})
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(productList);
+});
 
-router.post(`/`, (req, res) =>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
+router.post(`/`, async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid Category");
 
-    product.save().then((createdProduct=> {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-})
+  let product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
+  });
 
-module.exports =router;
+  product = await product.save();
+
+  if (!product) return res.status(500).send("The product cannot be created");
+
+  res.send(product);
+});
+module.exports = router;
