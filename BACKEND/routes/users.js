74522477219c09d79cb8bd8393142805fcba59e0
Muhammad <kashfahmed72@gmail.com>
2021-01-06 17:31:39 +0500

    USER Model & Schema

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -1,14 +1,33 @@
-const {User} = require('../models/user');
-const express = require('express');
+const { User } = require("../models/user");
+const express = require("express");
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+router.get(`/`, async (req, res) => {
+  const userList = await User.find();
 
-    if(!userList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(userList);
-})
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
 
-module.exports =router;
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  })
+
+  user = await user.save();
+
+  if (!user) return res.status(400).send("the user cannot be created!");
+  res.send(user);
+});
+
+module.exports = router;
