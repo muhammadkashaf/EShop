8e8d8e860ad20acb863e287bb4672fe52cf310f4
Muhammad <kashfahmed72@gmail.com>
2021-01-06 17:40:39 +0500

    Hashing the User Password

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -1,6 +1,7 @@
 const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 router.get(`/`, async (req, res) => {
   const userList = await User.find();
@@ -15,14 +16,15 @@ router.post("/", async (req, res) => {
   let user = new User({
     name: req.body.name,
     email: req.body.email,
-    passwordHash: req.body.passwordHash,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
+    street: req.body.street,
     apartment: req.body.apartment,
     zip: req.body.zip,
     city: req.body.city,
     country: req.body.country,
-  })
+  });
 
   user = await user.save();
 
